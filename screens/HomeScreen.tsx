f2d9f073dcf59ea4d17ff40d5fbd3f6bb325a3ca
lcrawshaw <lcrawshaw21@mail.wou.edu>
2022-03-11 14:09:22 -0800

    first commit

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Button} from 'react-native'
+import { NativeStackHeaderProps } from "@react-navigation/native-stack"
+
+
+export default function HomeScreen({navigation}: NativeStackHeaderProps) {
+
+    return (
+        <View>
+            <Text>I am home screen yeah</Text>
+        </View>
+    )
+}
