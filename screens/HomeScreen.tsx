475d3f1530ead14168a053f2fcdb07866b3c81d3
lcrawshaw <lcrawshaw21@mail.wou.edu>
2022-03-12 17:43:07 -0800

    added styling to home page

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,24 +1,18 @@
 import { View, Text, StyleSheet, FlatList } from 'react-native'
 import { NativeStackHeaderProps } from "@react-navigation/native-stack"
 import data from "../data.json"
-
+import { Workout } from '../types/data'
+import WorkoutItem from "../components/WorkoutItem"
 
 export default function HomeScreen({navigation}: NativeStackHeaderProps) {
 
-    const renderItem = ({item}: {item :any}) => {
-        return (
-            <View>
-                <Text>{item.name} - {item.difficulty}</Text>  
-            </View>  
-        )
-    }
-
     return (
         <View style={styles.container}>
+            <Text style={styles.header}>New Workouts</Text>
             <FlatList
-                data={data}
+                data={data as Workout[]}
                 keyExtractor={item => item.slug}
-                renderItem={renderItem}
+                renderItem={WorkoutItem}
             />
         </View>
     )
@@ -26,6 +20,12 @@ export default function HomeScreen({navigation}: NativeStackHeaderProps) {
 
 const styles = StyleSheet.create({
     container: {
-        padding: 20
+        padding: 20,
+        flex: 1,
+    },
+    header: {
+        fontSize: 20,
+        marginBottom: 20,
+        fontWeight: "bold",
     }
 })
